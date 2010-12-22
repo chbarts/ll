5a69c2d5214d01f73754a34e66a7509cd6d379b1
Chris Barts <chbarts@gmail.com>
2010-12-22 15:58:46 -0700

    Add a new demo program. I'm bored. 	new file:   cat.c

---
+++ cat.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "handle_ferr.h"
+#include "ggets.h"
+#include "ll3.h"
+
+static void *print(int tag, void *lp)
+{
+    char *ln = lp;
+
+    printf("%d: %s\n", tag, ln);
+
+    return lp;
+}
+
+static void dofile(char *pname, char *fname, FILE * inf)
+{
+    ll *beg = NULL;
+    char *ln;
+
+    while (fggets(&ln, inf) != EOF) {
+        if (!(beg = new_node((int) strlen(ln), ln, beg))) {
+            free_all_nodes(free_all_data(beg));
+            fprintf(stderr, "%s: new_node() failed on %s\n", pname, fname);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    puts(fname);
+
+    free_all_nodes(free_all_data(map(reverse(beg), print)));
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *inf;
+    int i;
+
+    if (argc == 1) {
+        dofile(argv[0], "stdin", stdin);
+
+        if (ferror(stdin)) {
+            handle_ferr("stdin", argv[0]);
+        }
+
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++) {
+        if (!(inf = fopen(argv[i], "r"))) {
+            handle_ferr(argv[i], argv[0]);
+            continue;
+        }
+
+        dofile(argv[0], argv[i], inf);
+
+        if (ferror(inf)) {
+            handle_ferr(argv[i], argv[0]);
+        }
+
+        if (fclose(inf)) {
+            handle_ferr(argv[i], argv[0]);
+        }
+    }
+
+    return 0;
+}
